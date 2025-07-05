56362227b2688e42cd1ed9e1d08414033f4ec20a
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-07-05 10:29:05 -0300

    feat: integrate all components in App.vue with navigation system

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import Navbar from './components/Navbar.vue'
+import Dashboard from './components/Dashboard.vue'
+import Patients from './components/Patients.vue'
+import MealPlans from './components/MealPlans.vue'
+import NutritionCalculator from './components/NutritionCalculator.vue'
+
+const currentView = ref('dashboard')
+
+const views = {
+  dashboard: Dashboard,
+  patients: Patients,
+  mealPlans: MealPlans,
+  calculator: NutritionCalculator
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="min-h-screen bg-gray-50">
+    <Navbar @change-view="currentView = $event" :active-view="currentView" />
+    
+    <main class="container mx-auto px-4 py-8">
+      <component :is="views[currentView]" />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+/* Estilos globais podem ser adicionados aqui se necessário */
 </style>
